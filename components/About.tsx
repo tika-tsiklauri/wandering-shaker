09bd3d2d544dc80fccc68299ae28871f17b55872
Tika Tsiklauri <tika1114@gmail.com>
2026-06-22 13:02:47 -0400

    text alignment

--- components/About.tsx
+++ components/About.tsx
@@ -115,7 +115,7 @@ function IntroHero({
 
       {/* Gentle vertical gradient */}
       <div className="absolute inset-0 bg-gradient-to-b from-black/10 via-transparent to-black/8" />
-      <div className="relative mx-auto mt-34 flex min-h-[100svh] max-w-6xl items-end px-5 pb-24 md:justify-end md:px-8 md:pb-20">
+      <div className="relative mx-auto mt-34 flex min-h-[100svh] max-w-6xl items-end px-5 pb-2 md:justify-end md:px-8 md:pb-20">
         {" "}
         <div className="max-w-2xl md:max-w-xl lg:mr-8">
           <p className="text-xs tracking-[0.18em] uppercase text-[var(--color-linen)]/85">
